1fd4be7134fb243e329a54b84b105789166b5890
Gogiu23 <giuliandominici@gmail.com>
2024-05-06 19:20:22 +0200

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+function fish_greeting
+	random choice "Hello stranger"
+	fastfetch
+end
+set -U fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
